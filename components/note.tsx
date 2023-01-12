c2723f5590cf33095e85c91ef8a3beb3cd059102
chendaozhang <1205308740@qq.com>
2023-01-13 01:00:31 +0800

    完成一个有bug的瀑布流

--- components/note.tsx
+++ components/note.tsx
@@ -1,22 +1,34 @@
 import Image from 'next/image'
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react';
 import s from '../styles/components/note.module.scss'
 interface Prop {
     top: number,
     left: number,
     text: string,
-    changeText: Dispatch<SetStateAction<{ id: number; top: number; left: number; text: string; }[]>>
+    id: number,
+    height: number,
+    changeText: Dispatch<SetStateAction<{ id: number; top: number; left: number; text: string; height: number }[]>>
 }
 export default function Note(prop: Prop) {
+    const refDiv = useRef<HTMLDivElement>(null)
+    const [height, setHeight] = useState<number>()
+
     const changeText = (e: React.FormEvent<HTMLDivElement>): void => {
         console.log(e.currentTarget.innerText)
     }
+    const isNotFocus = () => {
+        if (refDiv.current) {
+            console.log(refDiv.current)
+            console.log(prop.id)
+            setHeight(refDiv.current.clientHeight)
+        }
+    }
     return (
-        <div className={s.wrapper} style={{ top: prop.top, left: prop.left }}>
+        <div className={s.wrapper} style={{ top: prop.top, left: prop.left, height: prop.height }} ref={refDiv}>
             <div className={s.navBar}>
                 <Image src="/close.svg" width={20} height={20} alt="close" priority property="true" />
             </div>
-            <div className={s.textarea} onInput={changeText} contentEditable dangerouslySetInnerHTML={{ __html: prop.text }} />
+            <div className={s.textarea} onInput={changeText} onBlur={isNotFocus} contentEditable dangerouslySetInnerHTML={{ __html: prop.text }} />
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,38 +5,72 @@ import NavBar from '../components/navBar'
 import Note from '../components/note'
 import s from '../styles/index.module.scss'
 
+interface noteListData {
+  width: number,
+  listCount: number,
+  gapWidth: number,
+  leftArray: number[]
+}
+
 export default function Home() {
   const baseList = [
-    { id: 1, top: 10, left: 20, text: '31231321' },
-    { id: 2, top: 10, left: 270, text: '64' },
-    { id: 3, top: 10, left: 530, text: '64654654' },
-    { id: 4, top: 10, left: 800, text: '13213' },
-    { id: 5, top: 300, left: 20, text: '654654' },
-    { id: 6, top: 300, left: 270, text: '654654' },
-    { id: 7, top: 300, left: 530, text: '1321321' },
-    { id: 8, top: 300, left: 800, text: '645654' },
-    { id: 9, top: 600, left: 20, text: '65465487' },
-    { id: 10, top: 600, left: 270, text: '4654654' },
-    { id: 11, top: 600, left: 800, text: '3232465464' }
+    { id: 1, top: 10, left: 20, text: '31231321', height: 100 },
+    { id: 2, top: 10, left: 270, text: '64', height: 150 },
+    { id: 3, top: 10, left: 530, text: '64654654', height: 200 },
+    { id: 4, top: 10, left: 800, text: '13213', height: 300 },
+    { id: 5, top: 300, left: 20, text: '654654', height: 150 },
+    { id: 6, top: 300, left: 270, text: '654654', height: 112 },
+    { id: 7, top: 300, left: 530, text: '1321321', height: 130 },
+    { id: 8, top: 300, left: 800, text: '645654', height: 140 },
+    { id: 9, top: 600, left: 20, text: '65465487', height: 200 },
+    { id: 10, top: 600, left: 270, text: '4654654', height: 180 },
+    { id: 11, top: 600, left: 800, text: '3232465464', height: 130 }
   ]
   const [list, setList] = useState(baseList)
-  const [width, setWidth] = useState<number>()
-  const [limitList, setLimitList] = useState<number>()
+  const [noteListData, setNoteListData] = useState<noteListData>({ width: 1000, listCount: 3, gapWidth: 10, leftArray: [0, 0, 0] })
   useEffect(() => {
-    setWidth(window.innerWidth)
-    window.addEventListener('resize', () => {
-      setWidth(window.innerWidth)
-    })
+    if (window.innerWidth) {
+      setNoteListData(list => ({ ...list, width: window.innerWidth }))
+      window.addEventListener('resize', () => {
+        setNoteListData(list => ({ ...list, width: window.innerWidth }))
+      })
+    }
   }, [])
   useEffect(() => {
-    console.log(width)
-    if (width) {
-      setLimitList(Math.floor(width - 20 / 250))
+    if (noteListData.width) {
+      setNoteListData(list => {
+        const count = Math.floor(list.width / 250)
+        const width = (list.width - 40 - count * 250) / count
+        let leftArray = []
+        let currentLeft = width
+        for (let i = 0; i < count; i++) {
+          leftArray.push(currentLeft)
+          currentLeft += width + 250
+        }
+        return { ...list, gapWidth: width, listCount: count, leftArray: leftArray }
+      })
     }
-  }, [width])
+  }, [noteListData.width])
   useEffect(() => {
+    const leftArray = noteListData.leftArray
+    let count = 0
+    let heightArray: number[] = []
+    const a = list.map(noteList => {
+      if (count < 4) {
+        heightArray.push(noteList.height)
+        ++count
+        return { ...noteList, left: leftArray[count - 1] }
+      } else {
+        const i = count % heightArray.length
+        const height = heightArray[i]
+        heightArray[i] += noteList.height + 10
+        ++count
+        return { ...noteList, top: height + 20, left: leftArray[i] }
+      }
+    })
+    setList(a)
 
-  }, [limitList])
+  }, [noteListData.gapWidth])
 
   return (
     <>
@@ -52,7 +86,7 @@ export default function Home() {
         </div>
         <NavBar />
         <div className={s.noteWrapper}>
-          {list.map(note => <Note key={note.id} changeText={setList} top={note.top} left={note.left} text={note.text} />)}
+          {list.map(note => <Note key={note.id} id={note.id} changeText={setList} top={note.top} left={note.left} text={note.text} height={note.height} />)}
         </div>
       </main>
     </>

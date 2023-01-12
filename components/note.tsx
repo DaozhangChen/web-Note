42c527b622faf17d867a2f82a08be53021c1f0cd
chendaozhang <1205308740@qq.com>
2023-01-12 17:23:35 +0800

    完成note可编辑

--- components/note.tsx
+++ components/note.tsx
@@ -3,8 +3,10 @@ import s from '../styles/components/note.module.scss'
 export default function Note() {
     return (
         <div className={s.wrapper}>
-            <div className={s.navBar}><Image src="/close.svg" width={20} height={20} alt="close" /></div>
-            <div>这里是主要内容</div>
+            <div className={s.navBar}>
+                <Image src="/close.svg" width={20} height={20} alt="close" />
+            </div>
+            <div className={s.textarea} contentEditable />
         </div>
     )
 }

05a6ff5c5f6db6c2c625667f04c0a9353139d94a
chendaozhang <1205308740@qq.com>
2023-01-13 21:07:38 +0800

    引入mock.js

--- pages/api/getList.ts
+++ pages/api/getList.ts
@@ -1,25 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import Mock from 'mockjs'
 
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    const mockData = Mock.mock({
+        'data|1-20': [{
+            'id|+1': 1,
+            'text|1-100': '321',
+            'height|100-500': 100
+        }]
+    })
     if (req.method === "GET") {
-        res.status(200).json({
-            data: [
-                { id: 1, text: '31231321', height: 100 },
-                { id: 2, text: '64', height: 150 },
-                { id: 3, text: '64654654', height: 200 },
-                { id: 4, text: '13213', height: 300 },
-                { id: 5, text: '654654', height: 150 },
-                { id: 6, text: '654654', height: 112 },
-                { id: 7, text: '1321321', height: 130 },
-                { id: 8, text: '645654', height: 140 },
-                { id: 9, text: '65465487', height: 200 },
-                { id: 10, text: '4654654', height: 180 },
-                { id: 11, text: '3232465464', height: 130 }
-            ]
-        })
+        res.status(200).json(mockData)
     }
 
 }

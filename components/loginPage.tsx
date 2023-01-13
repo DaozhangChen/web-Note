541410b3aeaa90b3dcb89fb65b65301fec569b06
chendaozhang <1205308740@qq.com>
2023-01-14 01:22:51 +0800

    完成登录页面

--- components/loginPage.tsx
+++ components/loginPage.tsx
@@ -2,30 +2,54 @@ import { createPortal } from "react-dom"
 import { createRoot } from "react-dom/client"
 import s from '../styles/components/LoginPage.module.scss'
 import Image from "next/image"
+import { useState } from "react"
 
 type Prop = {
     closeLoginPage: () => void,
     maskClick: () => void
 }
 export default function LoginPage(prop: Prop) {
+    const [isLogin, setIsLogin] = useState<boolean>(true)
+    const changeState = () => {
+        setIsLogin(!isLogin)
+    }
     return (
         createPortal(
-            <>
-                <div className={s.mask} onClick={prop.maskClick} />
-                <div className={s.mainWrapper}>
-                    <div className={s.title}>
-                        <div>
-                            <span>在此登录</span>
-                            <span className={s.registerText}>点此注册</span>
+            <>{
+                isLogin ? <>
+                    <div className={s.mask} onClick={prop.maskClick} />
+                    <div className={s.mainWrapper}>
+                        <div className={s.title}>
+                            <div>
+                                <span> 登 录 </span>
+                                <span className={s.registerText} onClick={changeState}>点此注册</span>
+                            </div>
+                            <Image src="/close.svg" width={30} height={30} alt="close" priority property="true" onClick={prop.closeLoginPage} />
                         </div>
-                        <Image src="/close.svg" width={30} height={30} alt="close" priority property="true" onClick={prop.closeLoginPage} />
+                        <form className={s.formWrapper}>
+                            <input title="username" type="text" placeholder="用户名" />
+                            <input title="userpassword" type="password" placeholder="密码" autoComplete="cc-number" />
+                            <button title="submit" type="submit">登  录</button>
+                        </form>
                     </div>
-                    <form className={s.formWrapper}>
-                        <input title="username" type="text" placeholder="用户名" />
-                        <input title="userpassword" type="password" placeholder="密码" autoComplete="cc-number" />
-                        <button title="submit" type="submit">登  录</button>
-                    </form>
-                </div>
+                </> :
+                    <><div className={s.mask} onClick={prop.maskClick} />
+                        <div className={s.mainWrapper}>
+                            <div className={s.title}>
+                                <div>
+                                    <span > 注 册 </span>
+                                    <span className={s.registerText} onClick={changeState}>点此登录</span>
+                                </div>
+                                <Image src="/close.svg" width={30} height={30} alt="close" priority property="true" onClick={prop.closeLoginPage} />
+                            </div>
+                            <form className={s.formWrapper}>
+                                <input title="username" type="text" placeholder="用户名" />
+                                <input title="userpassword" type="password" placeholder="密码" autoComplete="cc-number" />
+                                <button title="submit" type="submit">注  册</button>
+                            </form>
+                        </div></>
+            }
+
             </>, document.body)
     )
 }
